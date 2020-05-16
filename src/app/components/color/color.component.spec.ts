e1b202163a51eabcf0e5b81381270086e0755482
JGRC <rodriguezcarrerajose@gmail.com>
2020-05-16 15:35:42 -0400

    Modificacion de las pruebas unitarias

--- src/app/components/color/color.component.spec.ts
+++ src/app/components/color/color.component.spec.ts
@@ -1,10 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ColorComponent } from './color.component';
+import { ColoresService } from '../../services/colores.service';
 
 describe('ColorComponent', () => {
   let component: ColorComponent;
-  let fixture: ComponentFixture<ColorComponent>;
+  let service = new ColoresService(null);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,12 +15,11 @@ describe('ColorComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ColorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new ColorComponent(service)
+
   });
 
-  it('should create', () => {
+  it('Se debe crear el componente ColorComponent', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/pages/inicio/inicio.component.spec.ts
+++ src/app/pages/inicio/inicio.component.spec.ts
@@ -1,25 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
 
 import { InicioComponent } from './inicio.component';
+import { ColoresService } from '../../services/colores.service';
+import { from } from 'rxjs';
 
 describe('InicioComponent', () => {
   let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
+  let servicio = new ColoresService(null);
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ InicioComponent ]
-    })
-    .compileComponents();
+    component = new InicioComponent( servicio );
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InicioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('Componente inicio creado', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Se debe cargar los colores', () => {
+    
+    const colores = [ {
+      color: "#98B2D1",
+      id: 1,
+      name: "cerulean",
+      pantone_value: "15-4020",
+      year: 2000
+      },
+      {
+      color: "#C74375",
+      id: 2,
+      name: "fuchsia rose",
+      pantone_value: "17-2031",
+      year: 2001
+      } 
+    ]
+
+    spyOn( servicio, 'getColors' ).and.callFake( ()=>{
+
+      return from( [ colores ] )
+
+    })
+
+
+    component.ngOnInit();
+
+    expect( component.colores.length ).toBeGreaterThan(0);
+  });
+
+  it('Solo existen paginas mayores a 0', ()=>{
+
+    let clickAnterior = -1;
+  
+    component.selectedPage = 1;
+    component.pageEvent(clickAnterior)
+    
+    expect( component.selectedPage ).toEqual( 1 );
+
+  })
+
 });
